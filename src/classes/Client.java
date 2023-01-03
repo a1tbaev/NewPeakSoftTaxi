4ce62e99157794033b2f7c905eb5248119e1308a
Kanykei500 <askarbekovakanykei120@gmail.com>
2023-01-03 17:13:51 +0300

    added package impl and class client

--- src/classes/Client.java
+++ src/classes/Client.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Client {
-    private Long id;
+    private Long  id;
     private String fullName;
     private LocalDate dateOfBirth;
     private String phoneNumber;
