0335b3de3434c2c593a9af83b184b2144c2bbb04
Kanykei500 <askarbekovakanykei120@gmail.com>
2023-01-03 18:27:25 +0300

    added client service method

--- src/interfaces/impl/ClientServiceImpl.java
+++ src/interfaces/impl/ClientServiceImpl.java
@@ -1,4 +1,45 @@
 package interfaces.impl;
 
-public class ClientServiceImpl {
+import classes.Client;
+import classes.Taxi;
+import interfaces.ClientService;
+
+import java.util.List;
+import java.util.Map;
+
+public class ClientServiceImpl implements ClientService {
+    @Override
+    public String addClient(Client client) {
+        return null;
+    }
+
+    @Override
+    public String addClient(List<Client> clients) {
+        return null;
+    }
+
+    @Override
+    public List<Client> getClientByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Client removeClientById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Taxi orderTaxi(Long clientId, String taxiType) {
+        return null;
+    }
+
+    @Override
+    public Map<Integer, Client> getClientAge() {
+        return null;
+    }
+
+    @Override
+    public void universalSorting(String word) {
+
+    }
 }
