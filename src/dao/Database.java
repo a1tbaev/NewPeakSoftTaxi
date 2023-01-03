8a492a506ed30d7792399dda5b2474bfc293ed28
Kanykei500 <askarbekovakanykei120@gmail.com>
2023-01-03 17:31:22 +0300

    added getter setter,to string,constructor in license and database class

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -14,4 +14,43 @@ public class Database {
     Set<Client>clients = new LinkedHashSet<>();
     Set<Driver>drivers = new LinkedHashSet<>();
     Set<Taxi>taxiList = new LinkedHashSet<>();
+
+    public Database(Set<Client> clients, Set<Driver> drivers, Set<Taxi> taxiList) {
+        this.clients = clients;
+        this.drivers = drivers;
+        this.taxiList = taxiList;
+    }
+
+    public Set<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(Set<Client> clients) {
+        this.clients = clients;
+    }
+
+    public Set<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public void setDrivers(Set<Driver> drivers) {
+        this.drivers = drivers;
+    }
+
+    public Set<Taxi> getTaxiList() {
+        return taxiList;
+    }
+
+    public void setTaxiList(Set<Taxi> taxiList) {
+        this.taxiList = taxiList;
+    }
+
+    @Override
+    public String toString() {
+        return "Database{" +
+                "clients=" + clients +
+                ", drivers=" + drivers +
+                ", taxiList=" + taxiList +
+                '}';
+    }
 }
