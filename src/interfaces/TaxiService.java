1be905b2c3feecff1ca11a87e6b95ebc0eb72c20
BaytikTaalaybekov <baytiktaalaybekov@gmail.com>
2023-01-04 18:16:18 +0600

    added TaxiService fields upload

--- src/interfaces/TaxiService.java
+++ src/interfaces/TaxiService.java
@@ -1,13 +1,21 @@
 package interfaces;
 
+import classes.Taxi;
+import enums.TaxiType;
+
+import java.util.List;
+import java.util.Map;
+
 public interface TaxiService {
+    StringBuilder add(Taxi taxi);
 
-//    StringBuilder add(Taxi taxi);
-//
-//    StringBuilder add(List<Taxi> taxis);
-//    List<Taxi> findByInitialLetter(String model);
-//    Map<TaxiType, List<Taxi>> grouping();
-//    List<Taxi> filterByTaxiType(String taxiType);
-//    void update(Long id);
+    StringBuilder add(List<Taxi> taxis);
 
+    List<Taxi> findByInitialLetter(String model);
+
+    Map<TaxiType, List<Taxi>> grouping();
+
+    List<Taxi> filterByTaxiType(String taxiType);
+
+    void update(Long id);
 }
