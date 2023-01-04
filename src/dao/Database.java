0eed348b3542d9d114d8ae4bf1cca2a3d25535e0
a1tbaev <aitbaevadil17@gmail.com>
2023-01-04 23:29:35 +0600

    change fields in database

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -11,47 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 public class Database {
-    Set<Client>clients = new LinkedHashSet<>();
-    Set<Driver>drivers = new LinkedHashSet<>();
-    Set<Taxi>taxiList = new LinkedHashSet<>();
-
-    public Database(Set<Client> clients, Set<Driver> drivers, Set<Taxi> taxiList) {
-        this.clients = clients;
-        this.drivers = drivers;
-        this.taxiList = taxiList;
-    }
-    public Database(){};
-
-    public Set<Client> getClients() {
-        return clients;
-    }
-
-    public void setClients(Set<Client> clients) {
-        this.clients = clients;
-    }
-
-    public Set<Driver> getDrivers() {
-        return drivers;
-    }
-
-    public void setDrivers(Set<Driver> drivers) {
-        this.drivers = drivers;
-    }
-
-    public Set<Taxi> getTaxiList() {
-        return taxiList;
-    }
-
-    public void setTaxiList(Set<Taxi> taxiList) {
-        this.taxiList = taxiList;
-    }
-
-    @Override
-    public String toString() {
-        return "Database{" +
-                "clients=" + clients +
-                ", drivers=" + drivers +
-                ", taxiList=" + taxiList +
-                '}';
-    }
+    public Set<Client>clients = new LinkedHashSet<>();
+    public Set<Driver>drivers = new LinkedHashSet<>();
+    public Set<Taxi>taxiList = new LinkedHashSet<>();
 }
--- src/interfaces/impl/DriverServiceImpl.java
+++ src/interfaces/impl/DriverServiceImpl.java
@@ -1,5 +1,64 @@
 package interfaces.impl;
 
-public class DriverServiceImpl {
+import classes.Driver;
+import dao.Database;
+import interfaces.DriverService;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class DriverServiceImpl implements DriverService {
+    List<Driver>driverList = new LinkedList<>();
+    Driver activeDriver = null;
+
+    @Override
+    public Driver add(Driver driver) {
+        this.activeDriver = driver;
+        return activeDriver;
+    }
+
+    @Override
+    public List<Driver> add(List<Driver> drivers) {
+        driverList.addAll(drivers);
+        return driverList;
+    }
+
+    @Override
+    public Driver findById(Long id) {
+        for(Driver driver : this.driverList){
+            if(Objects.equals(driver.getId(), id)){
+                return driver;
+            }else{
+                System.out.println("wrong id!!");
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Driver> findByName(String name) {
+        return driverList.stream().filter(s -> s.getName().equals(name)).toList();
+    }
+
+    @Override
+    public String assignTaxiToDriver(String taxiName, Long driverId) {
+        Database database = new Database();
+        return null;
+    }
+
+    @Override
+    public String changeTaxiOrDriver(Long driverId, Long taxiId) {
+        return null;
+    }
+
+    @Override
+    public List<Driver> getDriverByTaxiModel(String model) {
+        return null;
+    }
+
+    @Override
+    public void update(String driverName) {
+
+    }
 }
