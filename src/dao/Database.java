303da79ca4bc6b3a5c8893dd7d71c300d079e697
a1tbaev <aitbaevadil17@gmail.com>
2023-01-04 17:01:06 +0600

    added class database

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -1,4 +1,17 @@
 package dao;
 
+import classes.Client;
+import classes.Driver;
+import classes.Taxi;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Database {
+    Set<Client>clients = new LinkedHashSet<>();
+    Set<Driver>drivers = new LinkedHashSet<>();
+    Set<Taxi>taxiList = new LinkedHashSet<>();
 }
