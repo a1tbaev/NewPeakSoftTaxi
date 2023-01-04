c75cd09fd28f7419c1f52f5ff5f625da774f158f
kanykei <kasdasfdnykei@gmail.com>
2023-01-04 18:46:08 +0300

    complate main class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import classes.Client;
 import classes.Taxi;
 import enums.TaxiType;
+import interfaces.impl.ClientServiceImpl;
 import interfaces.impl.TaxiServiceImpl;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        ClientServiceImpl clientServiceimpl =new ClientServiceImpl();
+        List<Client>clients=new ArrayList<>(Arrays.asList(
+                new Client(11L,"Kanykei Askarbekova",LocalDate.of(2003,3,18),
+                        "0990128880",new BigDecimal(1000)),
+                new Client(12L,"Adil Aitbaev",LocalDate.of(2004,2,15),
+                        "0990128880",new BigDecimal(2000)),
+                new Client(13L,"Baytik Taalaybekov",LocalDate.of(2002,4,12),
+                        "0990128880",new BigDecimal(3000))));
+
+
+
+
 
 
     }
