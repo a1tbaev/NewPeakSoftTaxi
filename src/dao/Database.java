b4aa0897d9db247724ceadc90ec8cf4264e0d9ca
BaytikTaalaybekov <baytiktaalaybekov@gmail.com>
2023-01-05 18:10:37 +0600

    added getter and setter and finished taxi services. baytik ort. he is from bishkek. kochkor. jurogu talas dep sogot

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -14,4 +14,28 @@ public class Database {
     public Set<Client>clients = new LinkedHashSet<>();
     public Set<Driver>drivers = new LinkedHashSet<>();
     public Set<Taxi>taxiList = new LinkedHashSet<>();
+
+    public Set<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(Set<Client> clients) {
+        this.clients = clients;
+    }
+
+    public Set<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public void setDrivers(Set<Driver> drivers) {
+        this.drivers = drivers;
+    }
+
+    public Set<Taxi> getTaxiList() {
+        return taxiList;
+    }
+
+    public void setTaxiList(Set<Taxi> taxiList) {
+        this.taxiList = taxiList;
+    }
 }
